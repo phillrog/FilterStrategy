5b98a1d1357aa588b75d8c164a5a0a21d32409d5
fh.proger <phillipe.souza@rte.com.br>
2020-04-30 14:14:47 -0300

    Iniciando filtro

--- FilterStrategy.Bll/Implementation/GenerateInvoice.cs
+++ FilterStrategy.Bll/Implementation/GenerateInvoice.cs
@@ -1,17 +1,34 @@
 ﻿using FilterStrategy.Bll.Interface;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FilterStrategy.Bll
 {
-	public class GenerateInvoice
+	public class GenerateInvoice : IGenereateInvoice
 	{
 		private readonly IFreight _freight;
 
 		public GenerateInvoice(IFreight freight)
 		{
 			_freight = freight;
+		}
+
+		public async Task GenerateAsync(AutomaticBillingCustomerEvent filter)
+		{
+			/// ... Validations
+			
+			var freights = await _freight.GenerateFreightsModelForAutomaticBillingAsync(new FreightForBillingFilter { CustomerId = filter.CustomerId });
+
+			if (freights != null && freights.Count > 0)
+			{
+				List<FreightInvoiceGenerateModel> ableForBilling = new List<FreightInvoiceGenerateModel>();
+				List<FreightInvoiceGenerateModel> invalidForBilling = new List<FreightInvoiceGenerateModel>();
+			}
+
+			// ... continue process to generate invoice
 		}
 	}
 }
