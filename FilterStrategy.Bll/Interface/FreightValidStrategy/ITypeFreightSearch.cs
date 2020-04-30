185d34cd2724f9f790912e210421f62b35cd7c11
fh.proger <phillipe.souza@rte.com.br>
2020-04-30 18:00:32 -0300

    Remoção de modificador

--- FilterStrategy.Bll/Interface/FreightValidStrategy/ITypeFreightSearch.cs
+++ FilterStrategy.Bll/Interface/FreightValidStrategy/ITypeFreightSearch.cs
@@ -9,8 +9,8 @@ namespace FilterStrategy.Bll.Interface.FreightValidStrategy
 {
 	public interface ITypeFreightSearch
 	{
-		public List<BillingScheduleFrequencyEnum> Frequency { get; }
-		public List<BillingScheduleTypeEnum> Type { get; }
+		List<BillingScheduleFrequencyEnum> Frequency { get; }
+		List<BillingScheduleTypeEnum> Type { get; }
 		Task<(List<FreightInvoiceGenerateModel>, List<FreightInvoiceGenerateModel>)> FindAsync(List<FreightInvoiceGenerateModel> filter);
 	}
 }
